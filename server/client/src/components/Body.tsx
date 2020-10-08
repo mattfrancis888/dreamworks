8d3a6b79526abb3ebefc6c4eec291e89d8519531
Matthew Francis <mattfrancis888@gmail.com>
2020-10-08 15:04:09 -0400

    Created Banner section.

---
+++ server/client/src/components/Body.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Banner from "./Banner";
+const Body: React.FC<{}> = () => {
+    return <Banner />;
+};
+
+export default Body;
--- server/client/src/components/Routes.tsx
+++ server/client/src/components/Routes.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Header from "./Header";
-
+import Body from "./Body";
 const Routes: React.FC<{}> = () => {
     return (
-        <Header />
-        // <Switch>
-        //     <Route path="/" exact component={Body} />
-
-        // </Switch>
+        <React.Fragment>
+            <Header />
+            <Switch>
+                <Route path="/" exact component={Body} />
+            </Switch>
+        </React.Fragment>
     );
 };
 
