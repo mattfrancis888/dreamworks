5195756175679d76676a45a4ebd8d4aa47bb7146
Matthew Francis <mattfrancis888@gmail.com>
2020-10-08 19:16:42 -0400

    Created responsive carousel. Still need to design carousel.

--- server/client/src/components/Body.tsx
+++ server/client/src/components/Body.tsx
@@ -1,7 +1,44 @@
 import React from "react";
 import Banner from "./Banner";
+import trollsMovie from "../img/trollsMovie.jpg";
+import {
+    CarouselProvider,
+    Slider,
+    Slide,
+    ButtonBack,
+    ButtonNext,
+} from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
 const Body: React.FC<{}> = () => {
-    return <Banner />;
+    return (
+        <React.Fragment>
+            <Banner />
+            <h1 className="bodyMoviesTitle">Movies</h1>
+
+            <CarouselProvider
+                naturalSlideWidth={100}
+                naturalSlideHeight={50}
+                totalSlides={3}
+                className="moviesCarouselWrap"
+            >
+                <div className="sliderAndButtonWrap">
+                    <Slider>
+                        <Slide index={0}>
+                            <div className="carouselSlide">
+                                <img src={trollsMovie}></img>
+                                <img src={trollsMovie}></img>
+                                <img src={trollsMovie}></img>
+                            </div>
+                        </Slide>
+                        <Slide index={1}>I am the second Slide.</Slide>
+                        <Slide index={2}>I am the third Slide.</Slide>
+                    </Slider>
+                    <ButtonBack className="moviesBackButton">Back</ButtonBack>
+                    <ButtonNext className="moviesNextButton">Next</ButtonNext>
+                </div>
+            </CarouselProvider>
+        </React.Fragment>
+    );
 };
 
 export default Body;
