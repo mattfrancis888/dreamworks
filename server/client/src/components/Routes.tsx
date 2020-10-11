997eb8fbd4f642b61a6a7f080e1c4b0c456e08ff
Matthew Francis <mattfrancis888@gmail.com>
2020-10-11 19:31:04 -0400

    Created footer.

--- server/client/src/components/Routes.tsx
+++ server/client/src/components/Routes.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Header from "./Header";
 import Body from "./Body";
+import Footer from "./Footer";
 const Routes: React.FC<{}> = () => {
     return (
         <React.Fragment>
@@ -9,6 +10,7 @@ const Routes: React.FC<{}> = () => {
             <Switch>
                 <Route path="/" exact component={Body} />
             </Switch>
+            <Footer />
         </React.Fragment>
     );
 };
